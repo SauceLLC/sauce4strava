cd1cf79c8f05c97ffad05d068612c320c4836cc1
Justin Mayfield <tooker@gmail.com>
2020-01-06 18:21:01 -0700

    Enhance exports a bit.

--- src/site/export.js
+++ src/site/export.js
@@ -4,10 +4,22 @@ sauce.ns('export', function() {
     'use strict';
 
     class DOMSerializer {
-        constructor(name, start) {
-            this.name = name;
-            this.start = start;
+        constructor(name, desc, type, start) {
+            this.activity = {
+                name,
+                desc,
+                type,
+                start
+            };
             this.doc = new Document();
+        }
+
+        start() {
+            // virtual
+        }
+
+        loadStreams(streams) {
+            throw new Error("pure virtual");
         }
 
         addNodeTo(parent, name, textValue) {
@@ -18,15 +30,11 @@ sauce.ns('export', function() {
             return node;
         }
 
-        loadStreams(streams) {
-            throw new Error("pure virtual");
-        }
-
         toFile() {
             const heading = `<?xml version="${this.doc.xmlVersion}" encoding="${this.doc.inputEncoding}"?>\n`;
             return new File(
                 [heading + (new XMLSerializer()).serializeToString(this.doc)],
-                `${this.name}.${this.fileExt}`.replace(/\s/g, '_'),
+                `${this.activity.name}.${this.fileExt}`.replace(/\s/g, '_'),
                 {type: 'text/xml'}
             );
         }
@@ -35,39 +43,38 @@ sauce.ns('export', function() {
 
     class GPXSerializer extends DOMSerializer {
 
-        constructor(name, type, start) {
-            super(name, start);
+        start() {
             this.fileExt = 'gpx';
             this.rootNode = this.addNodeTo(this.doc, 'gpx');
             this.rootNode.setAttribute('creator', 'Strava Sauce');
             this.rootNode.setAttribute('version', '1.1');
             this.rootNode.setAttribute('xmlns', 'http://www.topografix.com/GPX/1/1');
-            this.rootNode.setAttribute('xmlns:gpxtpx', 'http://www.garmin.com/xmlschemas/TrackPointExtension/v1');
-            this.rootNode.setAttribute('xmlns:gpxx', 'http://www.garmin.com/xmlschemas/GpxExtensions/v3');
+            this.rootNode.setAttribute('xmlns:gpx3', 'http://www.garmin.com/xmlschemas/GpxExtensions/v3');
+            this.rootNode.setAttribute('xmlns:tpx1', 'http://www.garmin.com/xmlschemas/TrackPointExtension/v1');
             this.rootNode.setAttributeNS('http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation', [
-                'http://www.topografix.com/GPX/1/1',
-                'http://www.topografix.com/GPX/1/1/gpx.xsd',
-                'http://www.garmin.com/xmlschemas/GpxExtensions/v3',
-                'http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd',
-                'http://www.garmin.com/xmlschemas/TrackPointExtension/v1',
-                'http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd'
+                'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd',
+                'http://www.garmin.com/xmlschemas/GpxExtensions/v3 https://www8.garmin.com/xmlschemas/GpxExtensionsv3.xsd',
+                'http://www.garmin.com/xmlschemas/TrackPointExtension/v1 https://www8.garmin.com/xmlschemas/TrackPointExtensionv1.xsd',
             ].join(' '));
             const metadata = this.addNodeTo(this.rootNode, 'metadata');
-            this.addNodeTo(metadata, 'time', start.toISOString());
+            this.addNodeTo(metadata, 'time', this.activity.start.toISOString());
             const trk = this.addNodeTo(this.rootNode, 'trk');
-            this.addNodeTo(trk, 'name', name);
+            this.addNodeTo(trk, 'name', this.activity.name);
+            if (this.activity.desc) {
+                this.addNodeTo(trk, 'desc', this.activity.desc);
+            }
             // I can't find any docs on this enum.
             // I got these values by examining garmin output (strava uses numbers! lol).
             const trackTypeEnum = {
                 Ride: 'cycling',
                 Run: 'running'
             };
-            this.addNodeTo(trk, 'type', trackTypeEnum[type]);
+            this.addNodeTo(trk, 'type', trackTypeEnum[this.activity.type]);
             this.trkseg = this.addNodeTo(trk, 'trkseg');
         }
 
         loadStreams(streams) {
-            const startTime = this.start.getTime();
+            const startTime = this.activity.start.getTime();
             for (let i = 0; i < streams.time.length; i++) {
                 const point = this.addNodeTo(this.trkseg, 'trkpt');
                 if (streams.latlng) {
@@ -82,21 +89,22 @@ sauce.ns('export', function() {
                 }
                 const ext = this.addNodeTo(point, 'extensions');
                 if (streams.watts) {
+                    // NOTE: This is non standard and only works with GoldenCheetah.
                     const watts = streams.watts[i];
                     if (watts !== null) {
                         this.addNodeTo(ext, 'power', watts);
                     }
                 }
                 if (streams.temp || streams.heartrate || streams.cadence) {
-                    const tpx = this.addNodeTo(ext, 'gpxtpx:TrackPointExtension');
+                    const tpx = this.addNodeTo(ext, 'tpx1:TrackPointExtension');
                     if (streams.temp) {
-                        this.addNodeTo(tpx, 'gpxtpx:atemp', streams.temp[i]);
+                        this.addNodeTo(tpx, 'tpx1:atemp', streams.temp[i]);
                     }
                     if (streams.heartrate) {
-                        this.addNodeTo(tpx, 'gpxtpx:hr', streams.heartrate[i]);
+                        this.addNodeTo(tpx, 'tpx1:hr', streams.heartrate[i]);
                     }
                     if (streams.cadence) {
-                        this.addNodeTo(tpx, 'gpxtpx:cad', streams.cadence[i]);
+                        this.addNodeTo(tpx, 'tpx1:cad', streams.cadence[i]);
                     }
                 }
             }
@@ -106,18 +114,17 @@ sauce.ns('export', function() {
 
     class TCXSerializer extends DOMSerializer {
 
-        constructor(name, type, start) {
-            super(name, start);
+        start() {
             this.fileExt = 'tcx';
             this.rootNode = this.addNodeTo(this.doc, 'TrainingCenterDatabase');
             this.rootNode.setAttribute('xmlns', 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2');
-            this.rootNode.setAttribute('xmlns:up', 'http://www.garmin.com/xmlschemas/UserProfile/v2');
-            this.rootNode.setAttribute('xmlns:ax', 'http://www.garmin.com/xmlschemas/ActivityExtension/v2');
-            this.rootNode.setAttribute('xmlns:px', 'http://www.garmin.com/xmlschemas/ProfileExtension/v1');
-            this.rootNode.setAttribute('xmlns:ag', 'http://www.garmin.com/xmlschemas/ActivityGoals/v1');
+            this.rootNode.setAttribute('xmlns:up2', 'http://www.garmin.com/xmlschemas/UserProfile/v2');
+            // NOTE: This must be 'ns3' to support Strava's broken parser.
+            this.rootNode.setAttribute('xmlns:ns3', 'http://www.garmin.com/xmlschemas/ActivityExtension/v2');
             this.rootNode.setAttributeNS('http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation', [
-                'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
-                'http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd'
+                'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 https://www8.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd',
+                'http://www.garmin.com/xmlschemas/UserProfile/v2 https://www8.garmin.com/xmlschemas/UserProfileExtensionv2.xsd',
+                'http://www.garmin.com/xmlschemas/ActivityExtension/v2 https://www8.garmin.com/xmlschemas/ActivityExtensionv2.xsd',
             ].join(' '));
             const author = this.addNodeTo(this.rootNode, 'Author');
             author.setAttribute('xsi:type', 'Application_t');
@@ -126,37 +133,38 @@ sauce.ns('export', function() {
             this.addNodeTo(author, 'PartNumber', '867-5309');
             const build = this.addNodeTo(author, 'Build');
             const sauceVersion = this.addNodeTo(build, 'Version');
-            const [vmajor, vminor, bmajor, bminor] = sauce.version.split('.');
+            const [vmajor, vminor, bmajor] = sauce.version.split('.');
             this.addNodeTo(sauceVersion, 'VersionMajor', vmajor);
             this.addNodeTo(sauceVersion, 'VersionMinor', vminor);
             this.addNodeTo(sauceVersion, 'BuildMajor', bmajor);
-            this.addNodeTo(sauceVersion, 'BuildMinor', bminor || 0);
-
+            this.addNodeTo(sauceVersion, 'BuildMinor', 0);
             const activities = this.addNodeTo(this.rootNode, 'Activities');
             const activity = this.addNodeTo(activities, 'Activity');
             const sportEnum = {
                 Ride: 'Biking',
                 Run: 'Running'
             };
-            activity.setAttribute('Sport', sportEnum[type] || 'Other');
-            const startISOString = start.toISOString();
+            activity.setAttribute('Sport', sportEnum[this.activity.type] || 'Other');
+            const startISOString = this.activity.start.toISOString();
             this.addNodeTo(activity, 'Id', startISOString);  // Garmin does, so we will to.
+            const notes = this.activity.desc ?
+                `${this.activity.name}\n\n${this.activity.desc}` :
+                this.activity.name;
+            this.addNodeTo(activity, 'Notes', notes);
             const creator = this.addNodeTo(activity, 'Creator');
-            creator.setAttribute('xsi:type', 'Device_t');
+            creator.setAttribute('xsi:type', 'Device_t');  // Could maybe be Application_t too.
             this.addNodeTo(creator, 'Name', sauce.name);
             this.addNodeTo(creator, 'UnitId', 0);
             this.addNodeTo(creator, 'ProductId', 0);
-            creator.appendChild(sauceVersion);
-        
+            creator.appendChild(sauceVersion.cloneNode(/*deep*/ true));
             const lap = this.addNodeTo(activity, 'Lap');
             lap.setAttribute('StartTime', startISOString);
-            // Skipping: TotalTimeSeconds, DistenceMeters, Calories, Intensity, Cadence, Extensions
             this.addNodeTo(lap, 'TriggerMethod', 'Manual');
             this.track = this.addNodeTo(lap, 'Track');
         }
 
         loadStreams(streams) {
-            const startTime = this.start.getTime();
+            const startTime = this.activity.start.getTime();
             for (let i = 0; i < streams.time.length; i++) {
                 const point = this.addNodeTo(this.track, 'Trackpoint');
                 const t = (new Date(startTime + (streams.time[i] * 1000)));
@@ -178,15 +186,15 @@ sauce.ns('export', function() {
                     this.addNodeTo(hr, 'Value', streams.heartrate[i]);
                 }
                 if (streams.cadence) {
+                    // XXX Might be more accurate to use RunCadence ext for running types.
                     this.addNodeTo(point, 'Cadence', streams.cadence[i]);
                 }
                 if (streams.watts) {
                     const ext = this.addNodeTo(point, 'Extensions');
-                    const tpx = this.addNodeTo(ext, 'TPX');
-                    tpx.setAttribute('xmlns', 'http://www.garmin.com/xmlschemas/ActivityExtension/v2');
+                    const tpx = this.addNodeTo(ext, 'ns3:TPX');
                     const watts = streams.watts[i];
                     if (watts !== null) {
-                        this.addNodeTo(tpx, 'Watts', watts);
+                        this.addNodeTo(tpx, 'ns3:Watts', watts);
                     }
                 }
             }
