6aefbda9e1a28e586b71f5fe7d558ab7927f0514
Justin Mayfield <tooker@gmail.com>
2020-01-25 13:23:39 -0700

    Better support for remove athlete data

--- src/ext/adv_options.js
+++ src/ext/adv_options.js
@@ -8,24 +8,28 @@
     }
 
 
-    async function saveAthleteInfo(el, info) {
+    async function saveAthleteInfo(el) {
         const textareas = el.querySelectorAll('textarea.athlete-info');
         const status = document.getElementById('status');
         const errors = el.querySelectorAll('.error');
         for (const x of errors) {
             x.textContent = '';
         }
+        const athlete_info = {};
         for (const x of textareas) {
-            const athlete = info[x.id];
+            const val = x.value.trim();
+            if (!val) {
+                continue;  // removed
+            }
             try {
-                Object.assign(athlete, JSON.parse(x.value));
+                athlete_info[x.id] = JSON.parse(val);
             } catch(e) {
                 x.parentNode.querySelector('.error').textContent = e.toString();
-                status.textContent = `Error updating ${athlete.name} (${x.id})`;
+                status.textContent = `Error updating ${x.id}`;
                 return;
             }
         }
-        await sauce.storage.set('athlete_info', info);
+        await sauce.storage.set({athlete_info});
         status.textContent = 'Athlete options saved';
         await renderAthleteInfo(el);
         await sleep(8);
@@ -40,8 +44,11 @@
             const json = JSON.stringify(athlete, null, 2);
             const lines = json.split('\n');
             html.push(`
-                <div class="athlete-box">
-                    <div class="label">${athlete.name} (ID: ${id})</div>
+                <div class="athlete-box" data-athlete-id="${id}">
+                    <div class="label">
+                        ${athlete.name} (ID: ${id})
+                        <button class="remove red">Remove</button>
+                    </div>
                     <textarea rows="${lines.length}" class="athlete-info"
                               id="${id}">${json}</textarea>
                     <div class="error"></div>
@@ -49,6 +56,20 @@
             `);
         }
         el.innerHTML = html.join('');
+    }
+
+
+    function onEventDelegate(rootElement, evName, selector, callback) {
+        // redneck event delegation..
+        rootElement.addEventListener(evName, ev => {
+            if (ev.target && ev.target.closest) {
+                const delegateTarget = ev.target.closest(selector);
+                if (delegateTarget) {
+                    ev.delegateTarget = delegateTarget;
+                    return callback(ev);
+                }
+            }
+        });
     }
 
 
@@ -60,10 +81,16 @@
                 window.location.reload();
             });
         });
-        const info = await sauce.storage.get('athlete_info');
         const el = document.getElementById('athlete-list');
         await renderAthleteInfo(el);
-        document.getElementById('save').addEventListener('click', () => saveAthleteInfo(el, info));
+        onEventDelegate(el, 'click', '.label > button.remove', async ev => {
+            const id = ev.delegateTarget.closest('.athlete-box').dataset.athleteId;
+            const athlete_info = await sauce.storage.get('athlete_info');
+            delete athlete_info[id];
+            await sauce.storage.set({athlete_info});
+            await renderAthleteInfo(el);
+        });
+        document.getElementById('save').addEventListener('click', () => saveAthleteInfo(el));
     }
 
     document.addEventListener('DOMContentLoaded', main);
