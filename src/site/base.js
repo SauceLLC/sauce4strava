f8ccc85902be3da62c9397f220c36f4a6f3eca6a
Justin Mayfield <tooker@gmail.com>
2020-05-06 13:21:26 -0600

    Add theme switcher dev utility.

--- src/site/base.js
+++ src/site/base.js
@@ -15,3 +15,18 @@ sauce.ns = function(ns, callback) {
     }
     return offt;
 };
+
+
+sauce.theme = function(name) {
+    const doc = document.documentElement;
+    const classes = Array.from(doc.classList);
+    for (const x of classes) {
+        if (x.startsWith('sauce-theme-')) {
+            doc.classList.remove(x);
+        }
+    }
+    if (name) {
+        doc.classList.add('sauce-theme-enabled');
+        doc.classList.add(`sauce-theme-${name}`);
+    }
+};
