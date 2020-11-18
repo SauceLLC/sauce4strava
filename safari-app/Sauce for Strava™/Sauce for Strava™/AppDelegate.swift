4b7fe0dc615d57c725b7b181bad0c759d2692514
Justin Mayfield <tooker@gmail.com>
2020-11-17 18:57:27 -0700

    Add Safari xcode app (mostly stock)
    Use the web-extension convert tool to make this.  No idea what I'm
    doing, but it does basically function other than TCX export. Also
    resampling is disabled cause of missing OfflineAudioContext.

---
+++ safari-app/Sauce for Strava™/Sauce for Strava™/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Sauce for Strava™
+//
+//  Created by Justin Mayfield on 11/17/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
