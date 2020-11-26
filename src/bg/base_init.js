c8dfc1b8a9412b5a27a66d09bdb996e080470b59
Justin Mayfield <tooker@gmail.com>
2020-11-25 21:08:57 -0700

    Fix ga with safari in bg page

--- src/bg/base_init.js
+++ src/bg/base_init.js
@@ -1,2 +1,7 @@
 /* Since base is used as an eval source in preloading too, we must manually initialize it. */
 self.sauceBaseInit();
+if (self.browser.runtime.getURL('').startsWith('safari-web-extension:')) {
+    // Workaround for visibiltyState = 'prerender' causing GC to pause until unload
+    Object.defineProperty(document, 'visiblityState', {value: 'hidden'});
+    document.dispatchEvent(new Event('visibilitychange'));
+}
