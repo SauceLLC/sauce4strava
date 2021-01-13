ceb09708bfb8256ea4cd4375867ca1a45d78e8c9
Justin Mayfield <tooker@gmail.com>
2021-01-12 22:47:16 -0700

    Parasite complete.
    Fully replacing the error page for /sauce/performance/* now.  Now I just
    need to build and entire performance mgmt system.  Simple.

--- src/site/performance.js
+++ src/site/performance.js
@@ -3,11 +3,37 @@
 sauce.ns('performance', ns => {
     'use strict';
 
-
-    function load() {
-        console.warn("Let there be light");
+    // XXX Move from here and analysis to the template file..
+    const tplUrl = sauce.extUrl + 'templates';
+    const _tplCache = new Map();
+    const _tplFetching = new Map();
+    async function getTemplate(filename, localeKey) {
+        const cacheKey = '' + filename + localeKey;
+        if (!_tplCache.has(cacheKey)) {
+            if (!_tplFetching.has(cacheKey)) {
+                _tplFetching.set(cacheKey, (async () => {
+                    const resp = await fetch(`${tplUrl}/${filename}`);
+                    const tplText = await resp.text();
+                    localeKey = localeKey || 'analysis';
+                    _tplCache.set(cacheKey, sauce.template.compile(tplText, {localePrefix: `${localeKey}_`}));
+                    _tplFetching.delete(cacheKey);
+                })());
+            }
+            await _tplFetching.get(cacheKey);
+        }
+        return _tplCache.get(cacheKey);
     }
-    
+
+
+    async function load() {
+        console.warn("Let there be light");
+        const tpl = await getTemplate('performance.html');
+        const $replace = jQuery('#error404');
+        $replace.html(await tpl({}));
+        $replace.removeClass();  // removes all
+        $replace.attr('id', 'sauce-performance');
+    }
+
 
     return {
         load,
@@ -15,7 +41,7 @@ sauce.ns('performance', ns => {
 });
 
 
-addEventListener('DOMContentLoaded', async ev => {
+async function start() {
     if (sauce.testing) {
         return;
     }
@@ -25,4 +51,10 @@ addEventListener('DOMContentLoaded', async ev => {
         await sauce.report.error(e);
         throw e;
     }
-});
+}
+
+if (['interactive', 'complete'].indexOf(document.readyState) === -1) {
+    addEventListener('DOMContentLoaded', start);
+} else {
+    start();
+}
