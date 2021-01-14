98622135de350ba4893961d8690c126887c85752
Justin Mayfield <tooker@gmail.com>
2021-01-13 21:45:44 -0700

    Add sauce performance page to menu.

--- src/site/usermenu.js
+++ src/site/usermenu.js
@@ -5,7 +5,8 @@
 
     async function load() {
         try {
-            await _load();
+            _loadOptions();
+            _loadPerf();
         } catch(e) {
             await sauce.report.error(e);
             throw e;
@@ -13,9 +14,9 @@
     }
 
 
-    async function _load() {
-        const menuOptions = document.querySelector('#global-header .user-nav .user-menu .options');
-        if (!menuOptions) {
+    async function _loadOptions() {
+        const options = document.querySelector('#global-header .user-nav .user-menu .options');
+        if (!options) {
             return;
         }
         const anchor = document.createElement('a');
@@ -29,9 +30,23 @@
             sauce.report.event('UserMenu', 'options');
         });
         const item = document.createElement('li');
-        item.id = 'global-sauce-options-menu-item';
+        item.classList.add('sauce-options-menu-item');
         item.appendChild(anchor);
-        menuOptions.appendChild(item);
+        options.appendChild(item);
+    }
+
+    async function _loadPerf() {
+        const options = document.querySelector('#global-header .global-nav [data-log-category="training"] .options');
+        const anchor = document.createElement('a');
+        anchor.textContent = `Sauce ${await sauce.locale.getMessage('performance')}`;
+        const image = document.createElement('img');
+        image.src = sauce.extUrl + 'images/logo_horiz_128x48.png';
+        anchor.appendChild(image);
+        anchor.href = '/sauce/performance/';
+        const item = document.createElement('li');
+        item.classList.add('sauce-options-menu-item');
+        item.appendChild(anchor);
+        options.querySelector('li.premium').insertAdjacentElement('beforeBegin', item);
     }
 
     if (document.readyState === 'complete' || document.readyState === 'interactive') {
