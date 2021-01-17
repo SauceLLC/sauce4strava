087cd77d514d26c5591a42ae93e5ea4a34430067
Justin Mayfield <tooker@gmail.com>
2021-01-16 19:53:29 -0700

    Wire up nav controls for perf page.
    And of course more workshoping.

--- src/site/performance.js
+++ src/site/performance.js
@@ -1,4 +1,4 @@
-/* global sauce, jQuery, Chart */
+/* global sauce, jQuery, Chart, moment, regression */
 
 sauce.ns('performance', ns => {
     'use strict';
@@ -6,77 +6,6 @@ sauce.ns('performance', ns => {
     Chart.plugins.unregister(self.ChartDataLabels);  // Disable data labels by default.
 
     const DAY = 86400 * 1000;
-
-    function htmlTooltip(tooltipModel) {
-        // Tooltip Element
-        var tooltipEl = document.getElementById('chartjs-tooltip');
-
-        // Create element on first render
-        if (!tooltipEl) {
-            tooltipEl = document.createElement('div');
-            tooltipEl.id = 'chartjs-tooltip';
-            tooltipEl.innerHTML = '<table></table>';
-            document.body.appendChild(tooltipEl);
-        }
-
-        // Hide if no tooltip
-        if (tooltipModel.opacity === 0) {
-            tooltipEl.style.opacity = 0;
-            return;
-        }
-
-        // Set caret Position
-        tooltipEl.classList.remove('above', 'below', 'no-transform');
-        if (tooltipModel.yAlign) {
-            tooltipEl.classList.add(tooltipModel.yAlign);
-        } else {
-            tooltipEl.classList.add('no-transform');
-        }
-
-        function getBody(bodyItem) {
-            return bodyItem.lines;
-        }
-
-        // Set Text
-        if (tooltipModel.body) {
-            var titleLines = tooltipModel.title || [];
-            var bodyLines = tooltipModel.body.map(getBody);
-
-            var innerHtml = '<thead>';
-
-            titleLines.forEach(function(title) {
-                innerHtml += '<tr><th>' + title + '</th></tr>';
-            });
-            innerHtml += '</thead><tbody>';
-
-            bodyLines.forEach(function(body, i) {
-                var colors = tooltipModel.labelColors[i];
-                var style = 'background:' + colors.backgroundColor;
-                style += '; border-color:' + colors.borderColor;
-                style += '; border-width: 2px';
-                var span = '<span style="' + style + '"></span>';
-                innerHtml += '<tr><td>' + span + body + '</td></tr>';
-            });
-            innerHtml += '</tbody>';
-
-            var tableRoot = tooltipEl.querySelector('table');
-            tableRoot.innerHTML = innerHtml;
-        }
-
-        // `this` will be the overall tooltip
-        var position = this._chart.canvas.getBoundingClientRect();
-
-        // Display, position, and set styles for font
-        tooltipEl.style.opacity = 1;
-        tooltipEl.style.position = 'absolute';
-        tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
-        tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
-        tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
-        tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
-        tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
-        tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
-        tooltipEl.style.pointerEvents = 'none';
-    }
 
     // XXX Move from here and analysis to the template file..
     const tplUrl = sauce.extUrl + 'templates';
@@ -104,161 +33,265 @@ sauce.ns('performance', ns => {
         // NOTE: Activities should be in chronological order
         const days = [];
         let i = 0;
-        for (let ts = end; ts > start; ts -= DAY) {
+        for (let ts = start; ts < end; ts += DAY) {
             const acts = [];
             while (i < activities.length) {
                 const a = activities[i];
-                if (a.ts >= ts - DAY && a.ts < ts) {
+                if (a.ts >= ts && a.ts < ts + DAY) {
                     acts.push(a);
                     i++;
                 } else {
                     break;
                 }
             }
-            if (acts.length) {
-                days.push({ts, activities: acts});
-            }
+            days.push({ts, activities: acts});
         }
         return days;
     }
 
 
+    function activitiesByDayToDateStacks(data) {
+        // Turn the by-day data into an arrangment suitable for stacked chart datasets.
+        const stacks = [];
+        let level = 0;
+        while (true) {
+            const stack = [];
+            let added = 0;
+            for (const x of data) {
+                if (x.activities && x.activities.length > level) {
+                    stack.push({ts: x.ts, activity: x.activities[level]});
+                    added++;
+                } else {
+                    // Required to avoid time axis issues in chart.js
+                    stack.push({ts: x.ts});
+                }
+            }
+            if (!added) {
+                break;
+            }
+            stacks.push(stack);
+            level++;
+        }
+        return stacks;
+    }
+
+
     async function load() {
         const {tpl, athletes} = await initing;
-        const $replace = jQuery('#error404');
-        const now = Date.now();
-        const start = now - 86400 * 1000 * 30;
-        $replace.html(await tpl({athletes}));
-        $replace.removeClass();  // removes all
-        $replace.attr('id', 'sauce-performance');
-        const activities = await sauce.hist.getActivitiesForAthlete(athletes[0].id, {start});
-        const actsByDay = activitiesByDay(activities, start, now);
-        actsByDay.reverse();
-        const predDays = 7;
-        actsByDay.push({ts: now + (predDays * DAY), activities: []});
-
-        const tssCtx = document.querySelector('#tss').getContext('2d');
-        const tssChart = new Chart(tssCtx, {
-            type: 'bar',
-            plugins: [self.ChartRegressions],
-            options: {
-                scales: {
-                    xAxes: [{
-                        offset: true,  // Fixes clipping on start/end
-                        type: 'time',
-                        time: {
-                            isoWeekday: true,  // mon - sun
-                            minUnit: 'day',
-                        },
-                        gridLines: {
-                            display: false
-                        },
-                        ticks: {
-                            maxTicksLimit: 14,
-                        }
-                    }],
-                    yAxes: [{
-                        id: 'tss',
-                        type: 'linear',
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'TSS'
-                        },
-                        ticks: {
-                            min: 0,
-                            suggestedMax: 300,
-                        },
-                    }]
-                },
-                tooltips: {
-                    mode: 'index',
-                    enabled: false,
-                    custom: htmlTooltip,
-                    callbacks: {
-                        label: item => Math.round(item.value).toLocaleString(),
-                        footer: items => {
-                            const idx = items[0].index;
-                            const day = actsByDay[idx];
-                            return day.activities.map(x => `<a href="/activities/${x.id}">${x.name}</a>`).join('<br/>');
-                        }
-                    }
+        const maxEnd = Number(moment().endOf('day').toDate());
+        let athlete = athletes[0];  // XXX remember last or opt use URL param
+        let period = 30;  // days // XXX remember last and or opt use URL params
+        let periodEnd = maxEnd;  // opt use URL params
+        let periodStart = periodEnd - 86400 * 1000 * period;
+        const $page = jQuery('#error404');
+        $page.html(await tpl({athletes}));
+        $page.removeClass();  // removes all
+        $page.attr('id', 'sauce-performance');
+        $page.on('change', 'nav select[name="athlete"]', async ev => {
+            const athleteId = Number(ev.target.value);
+            for (const x of athletes) {
+                if (x.id === athleteId) {
+                    athlete = x;
+                    await render($page, athlete, periodStart, periodEnd);
+                    break;
                 }
-            },
-            data: {
-                datasets: [{
-                    label: 'TSS',
-                    yAxisID: 'tss',
-                    backgroundColor: '#ff4444',
-                    data: actsByDay.map(d => ({
-                        x: (new Date(d.ts)).toDateString(),
-                        y: sauce.data.sum(d.activities.map(a => a.stats ? (a.stats.tss || a.stats.tTss) : 0)),
-                    })),
-                }]
             }
         });
+        $page.on('change', 'main header select[name="period"]', async ev => {
+            period = Number(ev.target.value);
+            periodStart = periodEnd - (86400 * 1000 * period);
+            await render($page, athlete, periodStart, periodEnd);
+        });
+        $page.on('click', 'main header button.period', async ev => {
+            const next = ev.target.classList.contains('next');
+            periodEnd += period * DAY * (next ? 1 : -1);
+            periodStart += period * DAY * (next ? 1 : -1);
+            await render($page, athlete, periodStart, periodEnd);
+        });
+        await render($page, athlete, periodStart, periodEnd);
+    }
 
-        const hoursCtx = document.querySelector('#hours').getContext('2d');
-        const hoursChart = new Chart(hoursCtx, {
-            type: 'bar',
-            plugins: [self.ChartRegressions],
-            options: {
-                scales: {
-                    xAxes: [{
-                        offset: true,  // Fixes clipping on start/end
-                        type: 'time',
-                        time: {
-                            isoWeekday: true,  // mon - sun
-                            minUnit: 'day',
-                        },
-                        gridLines: {
-                            display: false
-                        },
-                        ticks: {
-                            maxTicksLimit: 14,
-                        }
-                    }],
-                    yAxes: [{
-                        id: 'time',
-                        type: 'linear',
-                        position: 'right',
-                        ticks: {
-                            min: 0,
-                            suggestedMax: 5 * 3600,
-                            stepSize: 3600,
-                            callback: v => sauce.locale.humanDuration(v)
-                        },
-                        gridLines: {
-                            display: false
-                        }
-                    }]
+
+    let tssChart;
+    let hoursChart;
+    async function render($page, athlete, start, end) {
+        const activities = await sauce.hist.getActivitiesForAthlete(athlete.id, {start, end});
+        activities.reverse();
+        const actsByDay = activitiesByDay(activities, start, end);
+        const actsDataStacks = activitiesByDayToDateStacks(actsByDay);
+        const $start = $page.find('header span.period.start');
+        const $end = $page.find('header span.period.end');
+        $start.text(moment(start).calendar());
+        $end.text(moment(end).format('ll'));
+        $page.find('button.period.next').prop('disabled', end >= Date.now());
+
+        function timeXAxis(extra) {
+            return Object.assign({
+                offset: true,  // Fixes clipping on start/end
+                type: 'time',
+                time: {
+                    minUnit: 'day',
+                    tooltipFormat: 'll',  // Jan 16, 2021
                 },
-                tooltips: {mode: 'index'}
-            },
-            data: {
-                datasets: [{
-                    label: 'Time',
-                    yAxisID: 'time',
-                    backgroundColor: '#33ffaa',
-                    data: actsByDay.map(d => ({
-                        x: (new Date(d.ts)).toDateString(),
-                        y: sauce.data.sum(d.activities.map(a => a.stats ? a.stats.activeTime : 0))
-                    })),
-                }]
-            }
-        });
+                gridLines: {
+                    display: false
+                },
+                ticks: {
+                    maxTicksLimit: 14,
+                }
+            }, extra);
+        }
 
-
-        const activityCtx = document.querySelector('#activity').getContext('2d');
-        const activityChart = new Chart(activityCtx, {
-            type: 'matrix',
-            data: {
-                labels: ['one', 'two', 'three', 'four', 'five'],
-                datasets: [{
-                    label: 'Activity',
-                    data: [100, 20, 33, 44, 100]
-                }]
+        async function chartClickHandler(ev) {
+            const chart = this;
+            const ds = chart.getElementsAtEvent(ev);
+            if (ds.length) {
+                const data = chart.actsByDay[ds[0]._index];
+                const t = await getTemplate('performance-details.html');
+                $page.find('aside.details').html(await t({
+                    moment,
+                    activities: data.activities
+                }));
             }
-        });
+        }
+
+        function tooltipSummaryHandler(items) {
+            const chart = this._chart;
+            const idx = items[0].index;
+            const day = chart.actsByDay[idx];
+            if (day.activities.length === 1) {
+                return `1 activity - click for details`;
+            } else {
+                return `${day.activities.length} activities - click for details`;
+            }
+        }
+
+        if (!tssChart) {
+            const ctx = document.querySelector('#tss').getContext('2d');
+            tssChart = new Chart(ctx, {
+                type: 'bar',
+                options: {
+                    plugins: {
+                        colorschemes: {
+                            scheme: 'brewer.Blues9',
+                            reverse: true,
+                            fillAlpha: 0.5,  // no effect with bar?
+                        }
+                    },
+                    legend: {display: false},
+                    scales: {
+                        xAxes: [timeXAxis({stacked: true})],
+                        yAxes: [{
+                            id: 'tss',
+                            type: 'linear',
+                            stacked: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'TSS'
+                            },
+                            ticks: {
+                                min: 0,
+                                suggestedMax: 300,
+                            },
+                        }]
+                    },
+                    tooltips: {
+                        mode: 'x',
+                        callbacks: {
+                            label: item => Math.round(item.value).toLocaleString(),
+                            footer: tooltipSummaryHandler
+                        }
+                    },
+                    onClick: chartClickHandler
+                }
+            });
+        }
+        tssChart.data.datasets = actsDataStacks.map((row, i) => ({
+            label: 'TSS', // currently hidden.
+            stack: 'tss',
+            yAxisID: 'tss',
+            borderColor: '#8888',
+            borderWidth: 1,
+            barPercentage: 0.9,
+            categoryPercentage: 1,
+            data: row.map(a => ({
+                x: a.ts,
+                y: (a.activity && a.activity.stats) ? (a.activity.stats.tss || a.activity.stats.tTss) : null,
+            })),
+        }));
+        tssChart.actsByDay = actsByDay;
+        tssChart.update();
+
+        if (!hoursChart) {
+            const ctx = document.querySelector('#hours').getContext('2d');
+            hoursChart = new Chart(ctx, {
+                type: 'bar',
+                options: {
+                    plugins: {
+                        colorschemes: {
+                            scheme: 'brewer.Reds9',
+                            reverse: true,
+                            fillAlpha: 0.5,  // no effect with bar?
+                        }
+                    },
+                    legend: {display: false},
+                    scales: {
+                        xAxes: [timeXAxis({stacked: true})],
+                        yAxes: [{
+                            id: 'hours',
+                            type: 'linear',
+                            stacked: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Duration'
+                            },
+                            ticks: {
+                                min: 0,
+                                suggestedMax: 5 * 3600,
+                                stepSize: 3600,
+                                callback: v => sauce.locale.humanDuration(v)
+                            }
+                        }]
+                    },
+                    tooltips: {
+                        mode: 'x',
+                        callbacks: {
+                            label: item => sauce.locale.humanDuration(item.value),
+                            footer: tooltipSummaryHandler
+                        }
+                    },
+                    onClick: chartClickHandler
+                }
+            });
+        }
+        const cleanActs = actsByDay.filter(x => x.activities);
+        const offts = cleanActs[0].ts;
+        const hoursReg = regression.polynomial(cleanActs.map(x => [
+            (x.ts - offts) / DAY,
+            sauce.data.sum(x.activities.map(a => a.stats && a.stats.activeTime ? a.stats.activeTime : 0))
+        ]), {order: 5, precision: 10});
+        hoursChart.data.datasets = [].concat(actsDataStacks.map((row, i) => ({
+            label: 'TSS', // currently hidden.
+            stack: 'hours',
+            yAxisID: 'hours',
+            borderColor: '#8888',
+            borderWidth: 1,
+            barPercentage: 0.9,
+            categoryPercentage: 1,
+            data: row.map(a => ({
+                x: a.ts,
+                y: (a.activity && a.activity.stats) ? a.activity.stats.activeTime : null,
+            })),
+        })), [{
+            type: 'line',
+            yAxisID: 'hours',
+            data: hoursReg.points.map(([day, val]) => ({
+                x: day * DAY + offts,
+                y: val
+            })),
+        }]);
+        hoursChart.actsByDay = actsByDay;
+        hoursChart.update();
     }
 
 
