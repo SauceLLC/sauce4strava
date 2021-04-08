0f7d8ddba25c65473ad758fe34e223367373f3bf
Justin Mayfield <tooker@gmail.com>
2021-04-08 15:11:41 -0600

    Functioning (but not done) webkit quota hack.
    Need to test fully on Safari now..

--- src/ext/webkit_db_size_hack.js
+++ src/ext/webkit_db_size_hack.js
@@ -9,29 +9,47 @@
         document.documentElement.classList.add('popup');
     }
 
-
-    function sleep(seconds) {
-        return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+    function estimateStorageSize(count) {
+        return count * 750;
     }
 
 
-    function onEventDelegate(rootElement, evName, selector, callback) {
-        // redneck event delegation..
-        rootElement.addEventListener(evName, ev => {
-            if (ev.target && ev.target.closest) {
-                const delegateTarget = ev.target.closest(selector);
-                if (delegateTarget) {
-                    ev.delegateTarget = delegateTarget;
-                    return callback(ev);
+    class DummyDatabase extends sauce.db.Database {
+        get migrations() {
+            return [{
+                version: 1,
+                migrate: (idb, t, next) => {
+                    idb.createObjectStore("dummy", {autoIncrement: true});
+                    next();
                 }
-            }
-        });
+            }];
+        }
+    }
+
+    class DummyStore extends sauce.db.DBStore {
+        constructor() {
+            super(new DummyDatabase('SauceWebkitQuotaHack'), 'dummy');
+        }
     }
 
 
-    async function reportEvent(eventCategory, eventAction, eventLabel) {
-        const t = await sauce.ga.getOrCreateTracker();
-        return t.send('event', {eventCategory, eventAction, eventLabel});
+    async function startAlloc(size) {
+        size *= 1024 * 1024;
+        const store = new DummyStore();
+        addEventListener('unload', () => {
+            console.warn("UNLOAD A BITCH");
+            indexedDB.deleteDatabase(store.db.name);
+        });
+        try {
+            const buf = new ArrayBuffer(Math.min(128 * 1024 * 1024, size));
+            for (let s = 0; s < size; s += buf.byteLength) {
+                console.warn("writing: ", buf.byteLength, s);
+                await store.put({buf});
+            }
+        } finally {
+            console.warn("DONE");
+            indexedDB.deleteDatabase(store.db.name);
+        }
     }
 
 
@@ -39,6 +57,19 @@
         document.querySelector('a.dismiss').addEventListener('click', () => {
             browser.tabs.update({active: true});  // required to allow self.close()
             self.close();
+        });
+        const estEl = document.querySelector('form.alloc-settings .est-space .usage');
+        const countEl = document.querySelector('form.alloc-settings input[name="athlete-count"]');
+        estEl.textContent = estimateStorageSize(Number(countEl.value)).toLocaleString();
+        countEl.addEventListener('input', ev => {
+            estEl.textContent = estimateStorageSize(Number(countEl.value)).toLocaleString();
+        });
+        const startEl = document.querySelector('form.alloc-settings input[name="start"]');
+        startEl.addEventListener('click', ev => {
+            ev.preventDefault();
+            countEl.disabled = (startEl.disabled = true);
+            document.documentElement.classList.add('active');
+            startAlloc(estimateStorageSize(Number(countEl.value)));
         });
         (await sauce.ga.getOrCreateTracker()).send('pageview');
     }
