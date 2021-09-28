5f42d774af2176cbb66f5ee7694e9a1a4da1e194
Justin Mayfield <tooker@gmail.com>
2021-09-27 18:56:59 -0600

    New safari project for dual ios/mac support.
    Will likely break everything.  ...sigh

---
+++ safari/Sauce for Strava™/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Justin Mayfield on 9/27/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
