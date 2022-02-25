a7c68274ab0a4ddbba23d0a268df19b9e4294c9c
Justin Mayfield <tooker@gmail.com>
2022-02-25 02:43:48 -0700

    Fix firefox base_init
    It actually doesn't have browser in the self namespace initially ??

--- src/common/base_init.js
+++ src/common/base_init.js
@@ -1,1 +1,1 @@
-self.sauceBaseInit(self.browser.runtime.id, self.browser.runtime.getURL(''), self.browser.runtime.getManifest());
+self.sauceBaseInit(browser.runtime.id, browser.runtime.getURL(''), browser.runtime.getManifest());
