f85db1bafd68698e193e5f458a058eebcff8326c
Justin Mayfield <tooker@gmail.com>
2024-09-16 22:06:38 -0600

    De-prototype base_init with warning if fallthrough is used

--- src/common/base_init.js
+++ src/common/base_init.js
@@ -4,7 +4,8 @@
     if (browser.runtime.getManifest) {
         manifest = browser.runtime.getManifest();
     } else {
-        manifest = {name: 'foo', version: '8.3.2'};
+        console.warn("Manifest unavailable");
+        manifest = {name: 'unset', version: '0.0.0'};
     }
     const {name, version} = manifest;
     self.sauceBaseInit(browser.runtime.id, browser.runtime.getURL(''), name, version);
