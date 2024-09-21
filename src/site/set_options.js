1d80b3c6cef52708d2c68dfc87e256dbc7064bea
Justin Mayfield <tooker@gmail.com>
2024-09-21 15:46:34 -0600

    degradeable site/set_options

--- src/site/set_options.js
+++ src/site/set_options.js
@@ -1,6 +1,8 @@
 (function() {
-    const params = JSON.parse(document.currentScript.dataset.params);
     self.sauce = self.sauce || {};
-    self.sauce.options = params.options;
-    Object.assign(self.sauce, params.patronVars);
+    if (document.currentScript.dataset.params) {
+        const params = JSON.parse(document.currentScript.dataset.params);
+        self.sauce.options = params.options;
+        Object.assign(self.sauce, params.patronVars);
+    }
 })();
