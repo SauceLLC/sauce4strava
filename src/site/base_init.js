91e13820e956bd45e9ca596f8f36cc8c0d2abd26
Justin Mayfield <tooker@gmail.com>
2024-09-25 11:19:51 -0600

    Workaround for firefox loading indeterminacy
    See: https://bugzilla.mozilla.org/show_bug.cgi?id=1920169
    
    Keeping js and css seperate in content_script entries restores loading
    order so site/base_init runs after the ext/boot process.

--- src/site/base_init.js
+++ src/site/base_init.js
@@ -1,27 +1,11 @@
-
-function init() {
-    const params = JSON.parse(document.documentElement.dataset.sauceBaseInitParams);
-    if (params) {
-        delete document.documentElement.dataset.sauceBaseInitParams;
-        self.sauceBaseInit(params.extId, params.extUrl, params.name, params.version);
-        self.saucePreloaderInit();
-    }
-}
-
-
 (function() {
     if (!document.documentElement.dataset.sauceBaseInitParams) {
-        // Firefox script loading is indeterminate...
+        // Firefox script loading can be indeterminate...
         // https://bugzilla.mozilla.org/show_bug.cgi?id=1920169
-        const mo = new MutationObserver(mutes => {
-            init();
-            mo.disconnect();
-        });
-        mo.observe(document.documentElement, {
-            attributes: true,
-            attributeFilter: ['data-sauce-base-init-params']
-        });
-    } else {
-        init();
+        throw new Error('Ext context has not loaded yet');
     }
+    const params = JSON.parse(document.documentElement.dataset.sauceBaseInitParams);
+    delete document.documentElement.dataset.sauceBaseInitParams;
+    self.sauceBaseInit(params.extId, params.extUrl, params.name, params.version);
+    self.saucePreloaderInit();
 })();
