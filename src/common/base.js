34350caf4d9b2fb59c1f0d6f8e88c9c072f28a9b
Justin Mayfield <tooker@gmail.com>
2024-09-28 12:18:20 -0600

    Add JS optimized hash func

--- src/common/base.js
+++ src/common/base.js
@@ -245,6 +245,33 @@ self.sauceBaseInit = function sauceBaseInit(extId, extUrl, name, version) {
     };
 
 
+    /*
+     * cyrb53 (c) 2018 bryc (github.com/bryc)
+     * License: Public domain. Attribution appreciated.
+     * A fast and simple 53-bit string hash function with decent collision resistance.
+     * Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
+     */
+    sauce.cyrb53 = function(str, seed=0) {
+        let h1 = 0xdeadbeef ^ seed;
+        let h2 = 0x41c6ce57 ^ seed;
+        for(let i = 0, ch; i < str.length; i++) {
+            ch = str.charCodeAt(i);
+            h1 = Math.imul(h1 ^ ch, 2654435761);
+            h2 = Math.imul(h2 ^ ch, 1597334677);
+        }
+        h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+        h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+        h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+        h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+        return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+    };
+
+
+    sauce.hash = function(str) {
+        return sauce.cyrb53(str);
+    };
+
+
     sauce.randomUUID = crypto.randomUUID ? () => crypto.randomUUID() : () =>
         ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
