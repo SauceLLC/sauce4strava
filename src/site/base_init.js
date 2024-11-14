4854c2efc651e1084a9ec0d7fbed76ed5f5926f3
Justin Mayfield <tooker@gmail.com>
2024-11-14 10:18:42 -0700

    Reintroduce bug workaround for startup order exec

--- src/site/base_init.js
+++ src/site/base_init.js
@@ -1,11 +1,25 @@
-(function() {
-    if (!document.documentElement.dataset.sauceBaseInitParams) {
-        // Firefox script loading can be indeterminate...
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=1920169
-        throw new Error('Ext context has not loaded yet');
-    }
+
+function init() {
     const params = JSON.parse(document.documentElement.dataset.sauceBaseInitParams);
     delete document.documentElement.dataset.sauceBaseInitParams;
     self.sauceBaseInit(params.extId, params.extUrl, params.name, params.version);
     self.saucePreloaderInit();
+}
+
+
+(function() {
+    if (!document.documentElement.dataset.sauceBaseInitParams) {
+        console.warn("Doing workaround for out of order startup bug:",
+                     "https://bugzilla.mozilla.org/show_bug.cgi?id=1920169");
+        const mo = new MutationObserver(mutes => {
+            init();
+            mo.disconnect();
+        });
+        mo.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ['data-sauce-base-init-params']
+        });
+    } else {
+        init();
+    }
 })();
